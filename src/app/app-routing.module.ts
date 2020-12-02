f87f29a450158cf8ee08e575c8c631e356aaf064
Antony Simiyu <antonymunyasia993@gmail.com>
2020-12-02 12:33:59 +0300

    Added Market template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HoodsComponent } from './components/hoods/hoods.component';
+import { MarketComponent } from './components/market/market.component';
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path:'all-hoods',
     component:HoodsComponent
+  },
+  {
+    path:'market',
+    component:MarketComponent
   }
 ];
 
