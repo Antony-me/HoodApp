2b54b6fd285f7f31050c39c75bfd950e05a207c9
andyjohn23 <andrewnick906@gmail.com>
2020-12-03 16:43:08 +0300

    created the signup, login components and added their routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { HoodsComponent } from './components/hoods/hoods.component';
+import { LoginComponent } from './components/login/login.component';
 import { MarketComponent } from './components/market/market.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   {
@@ -16,6 +18,14 @@ const routes: Routes = [
   {
     path:'market',
     component:MarketComponent
+  },
+  {
+    path:'signup',
+    component:SignupComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
   }
 ];
 
